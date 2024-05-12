c8802a909675ba1d45e75eb4befacee3f5b8c91d
Sorav-Kumar <soravrajput095@gmail.com>
2024-05-12 16:06:54 +0000

    dataConversion

---
+++ 01_basics/dataConversion.js
@@ -0,0 +1,39 @@
+// let score="33bjhjd";
+// let valueNumber=Number(score);
+// console.log(typeof score);// output string 
+// console.log(typeof(valueNumber));// output number
+// console.log(valueNumber);// NaN
+
+// score="33";
+// valueNumber=Number(score);
+// console.log(typeof score);// output string 
+// console.log(typeof(valueNumber));// output number
+// console.log(valueNumber);// 33
+
+// score="false";
+// valueNumber=Number(score);
+// console.log(typeof score);// output string 
+// console.log(typeof(valueNumber));// output number
+// console.log(valueNumber);// NaN
+
+// score=false;
+// valueNumber=Number(score);
+// console.log(typeof score);// output boolean 
+// console.log(typeof(valueNumber));// output number
+// console.log(valueNumber);// 0
+
+
+
+// isLoggedIn=1;
+// let booleanIsLoggedIN=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIN); // output true
+
+
+// isLoggedIn="kdjld";
+// let booleanIsLoggedIN=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIN); // output true
+
+
+isLoggedIn="";
+let booleanIsLoggedIN=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIN); // output false
