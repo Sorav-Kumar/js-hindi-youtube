d74da966a385066d4e702f478bc4177d3252933f
Sorav-Kumar <soravrajput095@gmail.com>
2024-05-12 12:37:17 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,9 @@
+const accountId=12334566;
+let emalId="sortouio@gmail.com";
+var accountCity="delhi";
+password="1589346";
+console.table([accountId,accountCity,emalId,password]);
+emalId="sorav@gmail.com";
+accountCity="noida";
+password= "7894561";
+console.table([accountId,accountCity,emalId,password]);
