27b55cbe2b743e9001c8ef35f1a8162967cdfb8c
Sorav-Kumar <soravrajput095@gmail.com>
2024-05-13 08:44:59 +0000

    operation

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,28 @@
+// let value=3;
+// let negValue=-value;
+// console.log(negValue);
+
+
+// console.log(2*2);       // output 4
+// console.log(2+5);      // output 7
+// console.log(5/2);       //output 2.5
+// console.log(5%2);       //output 1
+// console.log(4**3);      // output 64
+
+
+// let firstName="Sorav";
+// let LastName=" Kumar";
+// let fullName=firstName + LastName;
+// console.log(fullName);                  //output  Sorav Kumar
+
+
+
+
+// console.log("1"+2);      //Output 12     both 1 and 2 treat like string
+// console.log(1+"2");     // output 12   both 1 and 2 treat like string
+// console.log("1"+"2");   // output 12   both 1 and 2 treat like string
+// console.log("1"+2+3);   // output 123   All 1,2 and 3 treat like string
+
+
+// console.log(1+2+"5");      // output 35   1 and 2 are use like number and 5 is string
+// console.log(5+2+"3"+1+7);  // output  7317  5 and 2 are number and all other treat like string
