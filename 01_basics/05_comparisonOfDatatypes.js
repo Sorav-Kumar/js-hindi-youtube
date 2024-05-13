9338b1cddb8cafb13adbd65ba71d743945058faa
Sorav-Kumar <soravrajput095@gmail.com>
2024-05-13 09:54:17 +0000

    comparision of datatype

---
+++ 01_basics/05_comparisonOfDatatypes.js
@@ -0,0 +1,26 @@
+// console.log(2>1);   //output   true
+// console.log(2>3);   //output   false
+// console.log(5>=2);  //output   true
+// console.log(5==6);  //output   false
+// console.log(2!=1);      //output  true
+
+
+
+// // comparion of datatype of different datatype  (We should not use these type of conversion mostly)
+// console.log(null>0);   //output false
+// console.log(null>=0);   //output true
+// console.log(null!=0);   //output true
+// console.log(null==0);   //output false
+// console.log(null<0);   //output false
+
+
+// console.log(undefined>0);   //output false
+// console.log(undefined>=0);   //output false
+// console.log(undefined!=0);   //output true
+// console.log(undefined==0);   //output false
+// console.log(undefined<0);   //output false
+
+
+
+console.log("2"==2);   // output ture      here compaire only value
+console.log("2"===2);   //output false    here value and data type both are compaire
