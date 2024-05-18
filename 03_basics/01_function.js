835819440a6558ab1b67daf1b0751aad20337897
Sorav-Kumar <soravrajput095@gmail.com>
2024-05-18 17:16:57 +0000

    01 function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,25 @@
+// // // function  SumOfTwoNumber(number1,nnumber2)
+// // // {
+// // // console.log(number1+nnumber2) ;
+// // // }
+
+// // // SumOfTwoNumber(2,4);
+// // function  SumOfTwoNumber(number1,number2)
+// // {
+// // // let result =mnumber1+number2;
+// // // return result;
+// //  return number1+number2;
+// // }
+// // const result=SumOfTwoNumber(5,9);
+// // console.log("result:",result);
+
+// function loginUserMessage(UserName="sam"){
+// if(!UserName)
+//     {
+//         console.log("Pleas enter a username");
+//         return
+//     }
+
+//     return `${UserName} just logged in`
+// }
+// console.log(loginUserMessage());
