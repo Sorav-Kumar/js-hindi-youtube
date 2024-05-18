c10526d57035c2d08610a447e6830f882688172d
Sorav-Kumar <soravrajput095@gmail.com>
2024-05-18 17:34:34 +0000

    fjashajj

---
+++ 03_basics/02_function.js
@@ -0,0 +1,24 @@
+// function calculateCartPrice(val1,val2,...item1)
+// {
+// return item1
+// }
+// // console.log(calculateCartPrice(200,400,700,500));
+// const user={
+//     username:"sorav",
+//     price:199
+// }
+// function handleObject(anyObject){
+//     console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+// }
+// // handleObject(user);
+// handleObject({
+//     username:"kumar",
+//     price:600
+// });
+// }
+const myNewArray=[2,5,6,9,6]
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+console.log(returnSecondValue(myNewArray));
